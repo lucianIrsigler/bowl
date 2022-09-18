42c1f39c5f1f737f008f17c774ed2994ac01144c
CopyandPaste-png <79705748+CopyandPaste-png@users.noreply.github.com>
2022-09-18 16:24:02 +0200

    Add files via upload
    bowl

---
+++ bowl.h
@@ -0,0 +1,22 @@
+#ifndef BOWL_H
+#define BOWL_H
+
+//native types
+#define bowlI int
+#define bowlF float
+#define bowlD double
+#define bowlV void
+#define bowlA auto
+#define bowlC char
+#define bowlL long
+#define bowlSh short
+#define unBowl unsigned
+
+//Boolens
+#define bowlB bool
+#define BOWL true
+#define BOLW false
+
+#define bowlS std::string
+
+#endif
